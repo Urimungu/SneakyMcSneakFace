ef8f3647a13f1c60d0b28cb5b6824488e4e2ffc5
Uriel Mungiua <urimungu@uat.edu>
2020-01-29 12:25:15 -0700

    Enemy Patrol State
    I finished the Enemy Patrol State and It works so far. Need to add more things later.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
-using Random = System.Random;
 
 public class EnemyController : MonoBehaviour
 {
@@ -22,14 +18,15 @@ public class EnemyController : MonoBehaviour
 
     //States
     public enum States { Patrol, Chase, Searching };
-    public Transform[] searchPoint;
+    public List<Transform> searchPoint = new List<Transform>();
 
-
+    //Misc
     private States state;
     private bool isRunning = false;
     private bool isChasing = false;
     private bool inRange = false;
     public Transform target;
+    private float timer;
 
     private void Awake() {
         rb2d = GetComponent<Rigidbody2D>();
@@ -47,8 +44,7 @@ public class EnemyController : MonoBehaviour
         switch (state) {
             case States.Patrol:
                 Patrol();
-                Movement();
-            break;
+                break;
             case States.Searching:
                 break;
             case States.Chase:
@@ -58,15 +54,13 @@ public class EnemyController : MonoBehaviour
     }
 
     private void Patrol() {
-        if (inRange) {
-            print("Test");
+        if(inRange) {
             //Chooses a new target from the list and makes sure it's not the one that was already selected
-            int randomLocation = UnityEngine.Random.Range(0, searchPoint.Length);
-            if (target == searchPoint[randomLocation] && searchPoint.Length > 0)
-            {
-                if (randomLocation == 0)
+            int randomLocation = Random.Range(0, searchPoint.Count);
+            if(target == searchPoint[randomLocation] && searchPoint.Count > 0) {
+                if(randomLocation == 0)
                     randomLocation = 1;
-                else if (randomLocation == searchPoint.Length - 1)
+                else if(randomLocation == searchPoint.Count - 1)
                     randomLocation = 0;
                 else
                     ++randomLocation;
@@ -74,27 +68,32 @@ public class EnemyController : MonoBehaviour
 
             //Sets the location
             target = searchPoint[randomLocation];
+            inRange = false;
         }
-
-
+        if(timer < Time.time)
+            Movement();
     }
 
     private void Movement() {
         //Look at target and moves towards it
         Vector2 newPos = target.position - TF.position;
 
-        Vector3 newRot = Vector3.RotateTowards(TF.forward, newPos, RotationSpeed, 10);
-        TF.rotation = Quaternion.LookRotation(newRot);
+        float angle = Mathf.Atan2(newPos.y, newPos.x) * Mathf.Rad2Deg;
+        TF.rotation = Quaternion.AngleAxis(Mathf.LerpAngle(TF.eulerAngles.magnitude, angle, 0.1f), Vector3.forward);
 
-        //rb2d.AddForce(TF.forward * Speed);
+        rb2d.AddForce(TF.right * Speed);
 
         //Provides a counter speed to slow the enemy down
-        if (Mathf.Abs(rb2d.velocity.magnitude) > (isChasing ? runSpeedCap : SpeedCap))
-            rb2d.AddForce(-rb2d.velocity.normalized * (isChasing ? runSpeedCap : SpeedCap));
+        if (Mathf.Abs(rb2d.velocity.magnitude) > (isRunning ? runSpeedCap : SpeedCap)) {
+            rb2d.AddForce(-rb2d.velocity.normalized * (isRunning ? runSpeedCap : SpeedCap));
+        }
 
         //Slows down if it gets in range with its target
-        if (Mathf.Abs((target.position - TF.position).magnitude) < 0.1f && Mathf.Abs(rb2d.velocity.magnitude) > 0.1f) {
+        if (Mathf.Abs((target.position - TF.position).magnitude) < 0.5f && Mathf.Abs(rb2d.velocity.magnitude) > 0.1f) {
+            rb2d.velocity = Vector3.zero;
             rb2d.AddForce(-rb2d.velocity.normalized * stopTime);
+            timer = Time.time + Random.Range(2f, 6f);
+            inRange = true;
         }
     }
 }
