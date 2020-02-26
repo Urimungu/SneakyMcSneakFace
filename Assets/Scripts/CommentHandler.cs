6a11196ec83d7fa388d84fac58114f6a5a1fbd9a
Uriel Mungiua <urimungu@uat.edu>
2020-02-26 12:40:43 -0700

    Dialogue
    Speaks

---
+++ Assets/Scripts/CommentHandler.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CommentHandler : MonoBehaviour
+{
+    //Variables
+    public Text Text;
+    public Canvas CanvasHolder;
+    public float DisplayTime = 3;
+
+    //Messages
+    readonly List<string> _idleComments = new List<string> {
+        "This job is boring", "Why won't something interesting happen?", "Is cheese just the corpse of milk?", "Potatoes get soft in hot water but eggs get hard",
+        "I hate my life ngl", "Where is my tortilla?", "This program is broken.", "Wow, the same locations again!!", "Uggh!! These people have nothing valuable anyway",
+        "Imagine being a thief, HA! What a loser.", "Elijah! Where did you go?"
+    };
+    readonly List<string> _hearComments = new List<string> {
+        "What was that?!", "Huh!", "I heard something, hopefully it goes away.", "Footsteps!", "Is that the ghost?", "If this place is haunted, Im throwing hands",
+        "If there is a thief, I am programmed to shoot on sight.", "That noise sounded ugly", "Ughh! Really?", "Hmmm"
+
+    };
+    readonly List<string> _chaseComments = new List<string> {
+        "I will kill you!", "I'mma slide your grandma!", "Who are you?", "Why you gotta make me do my job?", "What are you doing here?", "Wait! I know you!",
+        "I'mma clap your cheeks!", "Finally, I get to use my gun!", "Eww, he's ugly!", "As long as it's not Elijah!", "I know where you live!", "Your face is repelling!"
+    };
+    readonly List<string> _losePlayerComments = new List<string> {
+        "Pfft.. whatever.", "Coward.", "I ain't finna chase his ass ngl.", "Get back here! I'm not programmed to chase you!", "Who cares lol", "Too ugly to chase",
+        "Terrible pursuit, 0/10", "Thank god, he went away", "Not my problem anymore.", "I hope he didn't take anything", "Whatever!", "Who the fuck was that?"
+    };
+
+    readonly List<string> _stopHearingComments = new List<string> {
+        "Must have been the wind", "Not my problem anymore.", "I don't really care what it was.", "Floor boards?", "I hope it's not that thief I saw earlier!",
+        "Thank God, I didn't wanna check that out.", "Who cares? Not me. I don't care.", "I hope that wasn't a ghost. I hate ghosts."
+
+    };
+
+    //Functions
+    private void Start() {
+        Text = transform.Find("Canvas").GetChild(0).GetComponent<Text>();
+        CanvasHolder = transform.Find("Canvas").GetComponent<Canvas>();
+        Text.text = "";
+
+    }
+
+    private void Update() {
+        CanvasHolder.transform.rotation = Quaternion.identity;
+    }
+
+    public void IdleComments() {
+        Text.text = _idleComments[Random.Range(0, _idleComments.Count)];
+        StopAllCoroutines();
+        StartCoroutine(stopMessage());
+    }
+    public void HearComments() {
+        Text.text = _hearComments[Random.Range(0, _hearComments.Count)];
+        StopAllCoroutines();
+        StartCoroutine(stopMessage());
+    }
+    public void ChaseComments() {
+        Text.text = _chaseComments[Random.Range(0, _chaseComments.Count)];
+        StopAllCoroutines();
+        StartCoroutine(stopMessage());
+    }
+    public void LosePlayerComments() {
+        Text.text = _losePlayerComments[Random.Range(0, _losePlayerComments.Count)];
+        StopAllCoroutines();
+        StartCoroutine(stopMessage());
+    }
+
+    public void StopHearingComments() {
+        Text.text = _stopHearingComments[Random.Range(0, _stopHearingComments.Count)];
+        StopAllCoroutines();
+        StartCoroutine(stopMessage());
+    }
+
+    IEnumerator stopMessage() {
+        yield return new WaitForSeconds(DisplayTime);
+        Text.text = "";
+    }
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,14 +5,16 @@ using Random = UnityEngine.Random;
 public class EnemyController : MonoBehaviour {
     //References
     public Sprite Walking, Chasing, Gun;
-    private Transform EnemyTransform;
-    private Rigidbody2D rb2d;
+    private Transform _enemyTransform;
+    private Rigidbody2D _rb2d;
+    private CommentHandler _commentHandler;
 
     //Stats
     public float Speed = 10, RunSpeed = 15;
     public float HearRadius;
+    public float ChaseRadius;
     public float SearchTime;
-    public float stopTime = 3;
+    public float StopTime = 3;
 
     //View Distance
     public float ViewDistance = 3;
@@ -24,44 +26,45 @@ public class EnemyController : MonoBehaviour {
     public List<List<Vector3>> SearchPoint = new List<List<Vector3>>();
 
     //Misc
-    private States state;
-    private bool isRunning;
-    private bool isChasing;
-    private bool inRange;
-    private bool movingForward = true;
-    private int currentPath;
-    private int pathState;
-    private float timer;
-    private Vector3 target;
-    private Vector3 playerLastPos;
+    private States _state;
+    private bool _isRunning;
+    private bool _isChasing;
+    private bool _inRange;
+    private bool _movingForward = true;
+    private int _currentPath;
+    private int _pathState;
+    private float _timer;
+    private Vector3 _target;
+    private Vector3 _playerLastPos;
 
 
     private void Awake() {
         //Sets the References
-        rb2d = GetComponent<Rigidbody2D>();
-        EnemyTransform = transform;
-        state = States.Patrol;
+        _commentHandler = GetComponent<CommentHandler>();
+        _rb2d = GetComponent<Rigidbody2D>();
+        _enemyTransform = transform;
+        _state = States.Patrol;
 
         //Checks to make sure that the points exist first
         if (transform.Find("PatrolLocations") == null)
             return;
 
         GetSearchPoints(transform.Find("PatrolLocations"));
-        target = SearchPoint[0][0];
+        _target = SearchPoint[0][0];
     }
 
     private void Update() {
         ChangeState();
     }
 
-    //This is the finite state-machine that the enemy will use
+    //This is the finite _state-machine that the enemy will use
     private void ChangeState() {
-        switch (state) {
+        switch (_state) {
             case States.Patrol:
                 Patrol();
-                //Stop timer
-                if(timer < Time.time)
-                    Movement();
+                //Stop _timer
+                if(_timer < Time.time)
+                    Movement(_target);
                 break;
             case States.Searching:
                 Searching();
@@ -74,59 +77,87 @@ public class EnemyController : MonoBehaviour {
 
     private void Chase(){
         //Face at the players
-        Vector2 newDirection = (GameManager.Manager.Player.transform.position - EnemyTransform.position).normalized;
-        EnemyTransform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg);
+
+        Vector2 newDirection = (GameManager.Manager.Player.transform.position - _enemyTransform.position).normalized;
+        var hit = Physics2D.Raycast(_enemyTransform.position, newDirection, 100, ViewLayerMask);
+
+        //Switches state
+        if (Time.time > _timer) {
+            _state = States.Searching;
+            GameManager.Manager.Heard++;
+            GameManager.Manager.Spotters--;
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = Chasing;
+            _timer = Time.time + SearchTime;
+            _isRunning = false;
+            _commentHandler.LosePlayerComments();
+            return;
+        }
+
+        //Skips the Rest if the player isn't in range
+        if (hit.transform.tag != "Player")
+            return;
+
+        _timer = Time.time + SearchTime;
+        _enemyTransform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg);
+
+        //If he sees the player and is chasing him
+        if ((GameManager.Manager.Player.transform.position - _enemyTransform.position).magnitude > ChaseRadius) {
+            Movement(GameManager.Manager.Player.transform.position, true);
+        }
 
     }
 
     //Looks around for the player, of he sees him then start following
     private void Searching() {
         //Looks at the Player's direction
-        Vector2 newDirection = (playerLastPos - EnemyTransform.position).normalized;
-        var slowRotation = Mathf.LerpAngle(EnemyTransform.eulerAngles.z, Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg, 0.3f);
-        EnemyTransform.rotation = Quaternion.Euler(0, 0, slowRotation);
+        Vector2 newDirection = (_playerLastPos - _enemyTransform.position).normalized;
+        var slowRotation = Mathf.LerpAngle(_enemyTransform.eulerAngles.z, Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg, 0.3f);
+        _enemyTransform.rotation = Quaternion.Euler(0, 0, slowRotation);
 
         //Checks for the player
         if (DetectPlayer()) {
             transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = Gun;
-            state = States.Chase;
+            _state = States.Chase;
             GameManager.Manager.Heard--;
             GameManager.Manager.Spotters++;
+            _isRunning = true;
+            _commentHandler.ChaseComments();
             return;
         }
 
         //If the player wasn't spotted in the amount of time
-        if (timer < Time.time) {
+        if (_timer < Time.time) {
             transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = Walking;
             GameManager.Manager.Heard--;
-            state = States.Patrol;
+            _state = States.Patrol;
+            _commentHandler.StopHearingComments();
         }
     }
 
     //Raycasts towards the player
     private bool DetectPlayer() {
         //Detects if the player is in front of the enemy
-        RaycastHit2D hit = Physics2D.Raycast(EnemyTransform.position, EnemyTransform.right, ViewDistance, ViewLayerMask);
+        RaycastHit2D hit = Physics2D.Raycast(_enemyTransform.position, _enemyTransform.right, ViewDistance, ViewLayerMask);
         if (hit.collider != null && hit.collider.tag == "Player")
             return true;
 
         //Detects if the player is far right
-        hit = Physics2D.Raycast(EnemyTransform.position, (EnemyTransform.right + (EnemyTransform.up / ViewAngle)), ViewDistance, ViewLayerMask);
+        hit = Physics2D.Raycast(_enemyTransform.position, (_enemyTransform.right + (_enemyTransform.up / ViewAngle)), ViewDistance, ViewLayerMask);
         if(hit.collider != null && hit.collider.tag == "Player")
             return true;
 
         //Detects if the player is far left
-        hit = Physics2D.Raycast(EnemyTransform.position, (EnemyTransform.right - (EnemyTransform.up / ViewAngle)), ViewDistance, ViewLayerMask);
+        hit = Physics2D.Raycast(_enemyTransform.position, (_enemyTransform.right - (_enemyTransform.up / ViewAngle)), ViewDistance, ViewLayerMask);
         if(hit.collider != null && hit.collider.tag == "Player")
             return true;
 
         //Detects if the player is mid-right
-        hit = Physics2D.Raycast(EnemyTransform.position, (EnemyTransform.right + (EnemyTransform.up / ViewAngle / 2)), ViewDistance, ViewLayerMask);
+        hit = Physics2D.Raycast(_enemyTransform.position, (_enemyTransform.right + (_enemyTransform.up / ViewAngle / 2)), ViewDistance, ViewLayerMask);
         if(hit.collider != null && hit.collider.tag == "Player")
             return true;
 
         //Detects if the player is mid-left
-        hit = Physics2D.Raycast(EnemyTransform.position, (EnemyTransform.right - (EnemyTransform.up / ViewAngle / 2)), ViewDistance, ViewLayerMask);
+        hit = Physics2D.Raycast(_enemyTransform.position, (_enemyTransform.right - (_enemyTransform.up / ViewAngle / 2)), ViewDistance, ViewLayerMask);
         if(hit.collider != null && hit.collider.tag == "Player")
             return true;
 
@@ -140,10 +171,11 @@ public class EnemyController : MonoBehaviour {
             return;
 
         //In range of Locator
-        if(inRange) {
+        if(_inRange) {
             //Sets the location
             GetNextLocation();
-            inRange = false;
+            _commentHandler.IdleComments();
+            _inRange = false;
         }
 
         //Breaks if there isn't a player or game manager in the scene so the game doesn't crash
@@ -151,77 +183,78 @@ public class EnemyController : MonoBehaviour {
             return;
 
         //Checks for the player
-        var hit = Physics2D.Raycast(EnemyTransform.position,
-            (EnemyTransform.position - GameManager.Manager.Player.transform.position).normalized, HearRadius);
+        var hit = Physics2D.Raycast(_enemyTransform.position,
+            (_enemyTransform.position - GameManager.Manager.Player.transform.position).normalized, HearRadius);
 
         //If the enemy hears the player by getting to close. If the player is behind a wall, he will get alerted
-        if ((EnemyTransform.position - GameManager.Manager.Player.transform.position).magnitude <= HearRadius &&
+        if ((_enemyTransform.position - GameManager.Manager.Player.transform.position).magnitude <= HearRadius &&
             (GameManager.Manager.Player.GetComponent<Rigidbody2D>().velocity.magnitude > 0.1f ||
             hit.collider != null && hit.collider.CompareTag("Player"))) {
-            timer = Time.time + SearchTime;
+            _timer = Time.time + SearchTime;
 
             //Stops the Enemy
-            rb2d.velocity = new Vector2(0, 0);
-            playerLastPos = GameManager.Manager.Player.transform.position;
+            _rb2d.velocity = new Vector2(0, 0);
+            _playerLastPos = GameManager.Manager.Player.transform.position;
 
-            //Changes state and sprite
-            state = States.Searching;
+            //Changes _state and sprite
+            _state = States.Searching;
             GameManager.Manager.Heard++;
             transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = Chasing;
+            _commentHandler.HearComments();
         }
     }
 
-    private void Movement() {
+    private void Movement(Vector3 target, bool chasing = false) {
         //Look at target and moves towards it
-        Vector2 newDirection = (target - EnemyTransform.position).normalized;
+        Vector2 newDirection = (target - _enemyTransform.position).normalized;
 
         //Moves the player and faces the direction
-        rb2d.velocity = newDirection * (isRunning ? RunSpeed : Speed);
-        var slowRotation = Mathf.LerpAngle(EnemyTransform.eulerAngles.z, Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg, 0.3f);
-        EnemyTransform.rotation = Quaternion.Euler(0, 0, slowRotation);
+        _rb2d.velocity = newDirection * (_isRunning ? RunSpeed : Speed);
+        var slowRotation = Mathf.LerpAngle(_enemyTransform.eulerAngles.z, Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg, 0.3f);
+        _enemyTransform.rotation = Quaternion.Euler(0, 0, slowRotation);
 
         //Stops if it gets in range with its target
-        if ((target - EnemyTransform.position).magnitude < 0.5f) {
-            rb2d.velocity = Vector3.zero;
-            timer = Time.time + Random.Range(2f, stopTime);
-            inRange = true;
+        if ((target - _enemyTransform.position).magnitude < 0.5f && !chasing) {
+            _rb2d.velocity = Vector3.zero;
+            _timer = Time.time + Random.Range(2f, StopTime);
+            _inRange = true;
         }
     }
 
     //Makes sure the Values Wrap around and don't return the same number
     private void GetNextLocation() {
         //Goes forwards
-        if ((movingForward && Random.Range(0, 100) > 15) || pathState == 0) {
+        if ((_movingForward && Random.Range(0, 100) > 15) || _pathState == 0) {
             //If hasn't reached the end of the list
-            if(SearchPoint[currentPath].Count - 1 != pathState)
-                pathState += 1;
+            if(SearchPoint[_currentPath].Count - 1 != _pathState)
+                _pathState += 1;
             else {
-                movingForward = false;
-                pathState -= 1;
+                _movingForward = false;
+                _pathState -= 1;
             }
 
-            target = SearchPoint[currentPath][pathState];
+            _target = SearchPoint[_currentPath][_pathState];
             return;
         }
 
         //Got back to the start of the room
-        if(pathState == 1) {
-            movingForward = true;
-            currentPath = Random.Range(0, SearchPoint.Count);
-            pathState = 0;
+        if(_pathState == 1) {
+            _movingForward = true;
+            _currentPath = Random.Range(0, SearchPoint.Count);
+            _pathState = 0;
 
-            target = SearchPoint[currentPath][pathState];
+            _target = SearchPoint[_currentPath][_pathState];
             return;
         }
 
 
         //Goes backwards
         if(Random.Range(0, 100) > 20)
-            pathState -= 1;
+            _pathState -= 1;
         else
-            pathState += 1;
+            _pathState += 1;
 
-        target = SearchPoint[currentPath][pathState];
+        _target = SearchPoint[_currentPath][_pathState];
     }
 
     //References all of the Children and adds them to the Vector3 List
